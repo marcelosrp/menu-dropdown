bfc163933ca7eb25ffadbe2f1dbaf97ac0609b7a
marcelo soares <soares.srp@gmail.com>
2020-07-29 15:45:54 -0300

    Menu dropdown

---
+++ assets/js/index.es6.js
@@ -0,0 +1,94 @@
+'use strict';
+
+$(() => {
+
+	$(window).on('load', function() {
+		$('#sidr').removeClass('d-none');
+
+		//mudando o display do menu dropdown para evitar que 'pisque' no load da pagina
+		$('.nav-dropdown').css('display', 'block');
+	});
+
+	$('.hamburger').sidr({
+		name: 'sidr',
+		side: 'right'
+	});
+
+	const Menu = {
+
+		linkMenu: null,
+		dropdown: null,
+		hamburger: null,
+		linkMenuMobile: null,
+		btnVoltarMenuMobile: null,
+
+		init: function() {
+			this.linkMenu = $('.has-children .link-menu');
+			this.dropdown = $('.menu-dropdown');
+			this.hamburger = $('.menu-mobile .hamburger');
+			this.linkMenuMobile = $('.has-children a');
+			this.btnVoltarMenuMobile = $('.voltar-menu');
+
+			this.linkMenu.on('click', function(e) {
+				e.preventDefault();
+
+				let dataMenu = $(this).attr('data-menu');
+				let dropdown = $('.menu-dropdown.'+dataMenu);
+				let selecionado = $(this);
+
+				Menu.mostraMenu(selecionado, dropdown);
+			});
+
+			this.hamburger.on('touchstart', function() {
+				Menu.ativaMobile();
+			});
+		},
+
+		mostraMenu: function(selecionado, dropdown) {
+			/* Desktop */
+			if( selecionado.next('ul').hasClass('is-hidden') ){
+				selecionado.addClass('selecionado').next('ul').removeClass('is-hidden').end().parent('ul');
+				selecionado.parent('.has-children').siblings('.has-children').children('ul').addClass('is-hidden').end().children('a').removeClass('selecionado');
+			}else{
+				selecionado.removeClass('selecionado').next('ul').addClass('is-hidden').end().parent('.has-children').parent('ul');
+			}
+		},
+
+		ativaMobile: function() {
+			var wrap = $('.wrap');
+
+			if( !this.hamburger.hasClass('is-active') ){
+
+				$(this.hamburger).addClass('is-active');
+
+				this.linkMenuMobile.on('click', function(e) {
+					let selecionado = $(this);
+
+					wrap.addClass('esconde');
+
+					if( selecionado.next('ul').hasClass('is-hidden') ){
+						selecionado.addClass('selecionado').next('ul').removeClass('is-hidden').end().parent('ul');
+					}
+				});
+
+				this.btnVoltarMenuMobile.on('click', function(e) {
+					e.preventDefault();
+
+					Menu.resetaMobile(this);
+				});
+
+			}else{
+				$(this.hamburger).removeClass('is-active');
+				Menu.resetaMobile(this);
+			}
+		},
+
+		resetaMobile: function(ativo) {
+			$(ativo).parent('ul').addClass('is-hidden').parent('.has-children').parent('ul');
+			$('.link-menu-mobile').removeClass('selecionado');
+			$('.wrap').removeClass('esconde');
+			$('.nav-dropdown-mobile').addClass('is-hidden');
+		}
+	}
+	Menu.init();
+});
